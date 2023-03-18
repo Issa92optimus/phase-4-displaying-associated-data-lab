16794013de6a8106d6371d4b89db5d6e8fed79b6
Issa Nyale <me@email.com>
2023-03-18 18:12:02 +0300

    completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+    def show
+        item = Item.find(params[:id])
+        render json: item, include: :user
+    end
+
+    def index
+        items = Item.all
+        render json: items, include: :user
+    end
 end
